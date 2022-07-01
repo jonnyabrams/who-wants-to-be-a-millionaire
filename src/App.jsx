726dba9563796a69318fc8e06c0ab160b11305bc
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-01 17:12:00 +0100

    Wrong answer triggers timeout, shows amount earned

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,29 @@ import { moneyPyramid, quizQuestions } from './data'
 const App = () => {
   const [questionNumber, setQuestionNumber] = useState(1)
   const [timeElapsed, setTimeElapsed] = useState(false)
+  const [amountEarned, setAmountEarned] = useState("£0")
 
   return (
     <div className="app">
       <div className="main">
-        <div className="top">
-          <div className="timer">30</div>
-        </div>
-        <div className="bottom">
-            <Quiz 
-              quizQuestions={quizQuestions} 
-              setTimeOut={setTimeElapsed}
-              questionNumber={questionNumber} 
-              setQuestionNumber={setQuestionNumber} 
-            />
-          </div>
+        { timeElapsed ? (
+          <h1>You earned: {amountEarned}</h1>
+        ) : (
+          <>
+            <div className="top">
+              <div className="timer">30</div>
+            </div>
+            <div className="bottom">
+              <Quiz 
+                quizQuestions={quizQuestions} 
+                setTimeElapsed={setTimeElapsed}
+                questionNumber={questionNumber} 
+                setQuestionNumber={setQuestionNumber} 
+              />
+            </div>
+          </>
+        )}
+        
       </div>
       <div className="pyramid">
         <ul className='money-list'>
