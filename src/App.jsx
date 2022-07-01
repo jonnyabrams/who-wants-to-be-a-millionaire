eeb2c7256218570d651d49604aadde05937e60c4
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-01 15:17:41 +0100

    Styled pyramid area of screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,26 @@ const App = () => {
   return (
     <div className="app">
       <div className="main">main</div>
-      <div className="pyramid">money</div>
+      <div className="pyramid">
+        <ul className='money-list'>
+          <li className='money-list-item'>
+            <span className='money-list-item-number'>4</span>
+            <span className='money-list-item-amount'>£400</span>
+          </li>
+          <li className='money-list-item'>
+            <span className='money-list-item-number'>3</span>
+            <span className='money-list-item-amount'>£300</span>
+          </li>
+          <li className='money-list-item'>
+            <span className='money-list-item-number'>2</span>
+            <span className='money-list-item-amount'>£200</span>
+          </li>
+          <li className='money-list-item'>
+            <span className='money-list-item-number'>1</span>
+            <span className='money-list-item-amount'>£100</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
