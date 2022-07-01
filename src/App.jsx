454b05d711761d8baea681658f30223d4db85e4e
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-01 15:05:51 +0100

    Styled main area of screen with b/g img + gradient

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="main">main</div>
+      <div className="pyramid">money</div>
+    </div>
+  );
+}
+
+export default App
