954bedc59b9ae0e57455b4938ac77c4608b0a420
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-02 12:18:00 +0100

    Update questions

--- src/data.js
+++ src/data.js
@@ -19,67 +19,331 @@ export const moneyPyramid = [
 export const quizQuestions = [
   {
     id: 1,
-    question: "Rolex is a company that specializes in what type of product?",
+    question: "Who is NOT an original member of Wu-Tang Clan?",
     answers: [
       {
-        text: "Phone",
+        text: "Ghostface Killah",
         correct: false,
       },
       {
-        text: "Watches",
+        text: "Cappadonna",
         correct: true,
       },
       {
-        text: "Food",
+        text: "Method Man",
         correct: false,
       },
       {
-        text: "Cosmetic",
+        text: "RZA",
         correct: false,
       },
     ],
   },
   {
     id: 2,
-    question: "When did the website `Facebook` launch?",
+    question: "The majority of Wu-Tang members are from which New York City Borough?",
     answers: [
       {
-        text: "2004",
+        text: "Staten Island",
         correct: true,
       },
       {
-        text: "2005",
+        text: "Brooklyn",
         correct: false,
       },
       {
-        text: "2006",
+        text: "Queens",
         correct: false,
       },
       {
-        text: "2007",
+        text: "Bronx",
         correct: false,
       },
     ],
   },
   {
     id: 3,
-    question: "Who played the character of harry potter in movie?",
+    question: "Who was the first Wu-Tang member to release a solo album?",
     answers: [
       {
-        text: "Johnny Deep",
+        text: "Method Man",
         correct: false,
       },
       {
-        text: "Leonardo Di Caprio",
+        text: "Ghostface Killah",
         correct: false,
       },
       {
-        text: "Denzel Washington",
-        correct: false,
-      },
-      {
-        text: "Daniel Red Cliff",
+        text: "GZA",
         correct: true,
+      },
+      {
+        text: "Raekwon",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "'Ghostface, catch the blast of a hype verse/My glock bursts, leave in a hearse, I did worse' are lyrics from which 36 Chambers track?",
+    answers: [
+      {
+        text: "Da Mystery of Chessboxin'",
+        correct: false,
+      },
+      {
+        text: "Bring Da Ruckus",
+        correct: true,
+      },
+      {
+        text: "Protect Ya Neck",
+        correct: false,
+      },
+      {
+        text: "C.R.E.A.M.",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Which two members most frequently collaborated which each other on other projects?",
+    answers: [
+      {
+        text: "Method Man and Redman",
+        correct: false,
+      },
+      {
+        text: "ODB and U-God",
+        correct: false,
+      },
+      {
+        text: "RZA and GZA",
+        correct: false,
+      },
+      {
+        text: "Ghostface Killah and Raekwon",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "Which member has a track named after him?",
+    answers: [
+      {
+        text: "Method Man",
+        correct: true,
+      },
+      {
+        text: "Ghostface Killah",
+        correct: false,
+      },
+      {
+        text: "Ol' Dirty Bastard",
+        correct: false,
+      },
+      {
+        text: "U-God",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "RZA did the soundtrack to which 2004 Hollywood blockbuster?",
+    answers: [
+      {
+        text: "Bad Boys II",
+        correct: false,
+      },
+      {
+        text: "Get Rich or Die Tryin'",
+        correct: false,
+      },
+      {
+        text: "You Got Served",
+        correct: false,
+      },
+      {
+        text: "Kill Bill",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "Which member had small roles in the movies 187 and Copland?",
+    answers: [
+      {
+        text: "Method Man",
+        correct: true,
+      },
+      {
+        text: "Ghostface Killah",
+        correct: false,
+      },
+      {
+        text: "GZA",
+        correct: false,
+      },
+      {
+        text: "Raekwon",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "Which member has a US album cover that depicts a black-hooded man delivering a vicious blow to the neck of his enemy with a sword while standing on what appears to be a chess board?",
+    answers: [
+      {
+        text: "Method Man",
+        correct: false,
+      },
+      {
+        text: "Ghostface Killah",
+        correct: false,
+      },
+      {
+        text: "GZA",
+        correct: true,
+      },
+      {
+        text: "Raekwon",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "What is U-God's most-used moniker?",
+    answers: [
+      {
+        text: "Osiris",
+        correct: false,
+      },
+      {
+        text: "Bobby Digital",
+        correct: false,
+      },
+      {
+        text: "The Fifth Brother",
+        correct: false,
+      },
+      {
+        text: "Golden Arms",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "'Raw I'ma give it to ya, wit' no trivia/Raw like cocaine straight from Bolivia' are lyrics rapped by which member?",
+    answers: [
+      {
+        text: "Inspectah Deck",
+        correct: false,
+      },
+      {
+        text: "Ghostface Killah",
+        correct: false,
+      },
+      {
+        text: "U-God",
+        correct: true,
+      },
+      {
+        text: "Raekwon",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "In which year did Raekwon release Only Built 4 Cuban Linx...?",
+    answers: [
+      {
+        text: "1992",
+        correct: false,
+      },
+      {
+        text: "1993",
+        correct: false,
+      },
+      {
+        text: "1994",
+        correct: false,
+      },
+      {
+        text: "1995",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "Which was the first Ghostface Killah solo album?",
+    answers: [
+      {
+        text: "Supreme Clientele",
+        correct: false,
+      },
+      {
+        text: "Bulletproof Wallets",
+        correct: false,
+      },
+      {
+        text: "Ironman",
+        correct: true,
+      },
+      {
+        text: "Fishscale",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question: "Which member composed the score for Ghost Dog: The Way of the Samurai?",
+    answers: [
+      {
+        text: "Method Man",
+        correct: false,
+      },
+      {
+        text: "Ghostface Killah",
+        correct: false,
+      },
+      {
+        text: "RZA",
+        correct: true,
+      },
+      {
+        text: "Raekwon",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question: "Which two members are the stars of stoner movie How High?",
+    answers: [
+      {
+        text: "Method Man and Redman",
+        correct: true,
+      },
+      {
+        text: "Ghostface Killah and Raekwon",
+        correct: false,
+      },
+      {
+        text: "RZA and GZA",
+        correct: true,
+      },
+      {
+        text: "ODB and U-God",
+        correct: false,
       },
     ],
   },
